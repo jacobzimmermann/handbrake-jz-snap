ac87df9e131ea9dd18bde370472da352537ba073
Jacob Zimmermann <ppa@jzimm.net>
2019-09-13 11:31:47 +1000

    New upstream version 1.2.2

--- snap/plugins/inbuild.py
+++ snap/plugins/inbuild.py
@@ -1,6 +1,6 @@
 # -*- Mode:Python; indent-tabs-mode:nil; tab-width:4 -*-
 #
-# Copyright (C) 2015, 2016 Canonical Ltd
+# Copyright (C) 2015-2016 Canonical Ltd
 # Copyright (C) 2016 Harald Sitter <sitter@kde.org>
 #
 # This program is free software: you can redistribute it and/or modify
@@ -42,55 +42,52 @@ In addition, this plugin uses the following plugin-specific keywords:
 import os
 import stat
 
-import snapcraft
+from snapcraft.plugins import make
 
 
-class AutotoolsPlugin(snapcraft.BasePlugin):
-
+class InbuildPlugin(make.MakePlugin):
     @classmethod
     def schema(cls):
         schema = super().schema()
-        schema['properties']['configflags'] = {
-            'type': 'array',
-            'minitems': 1,
-            'uniqueItems': True,
-            'items': {
-                'type': 'string',
-            },
-            'default': [],
+        schema["properties"]["configflags"] = {
+            "type": "array",
+            "minitems": 1,
+            "items": {"type": "string"},
+            "default": [],
         }
 
-        schema['properties']['install-via'] = {
-            'enum': ['destdir', 'prefix'],
-            'default': 'destdir',
+        schema["properties"]["install-via"] = {
+            "enum": ["destdir", "prefix"],
+            "default": "destdir",
         }
 
-        # Inform Snapcraft of the properties associated with building. If these
-        # change in the YAML Snapcraft will consider the build step dirty.
-        schema['build-properties'].extend(['configflags', 'install-via'])
+        schema["required"] = ["source"]
 
         return schema
 
+    @classmethod
+    def get_build_properties(cls):
+        # Inform Snapcraft of the properties associated with building. If these
+        # change in the YAML Snapcraft will consider the build step dirty.
+        return super().get_build_properties() + ["configflags", "install-via"]
+
     def __init__(self, name, options, project):
         super().__init__(name, options, project)
-        self.build_packages.extend([
-            'autoconf',
-            'automake',
-            'autopoint',
-            'libtool',
-            'make',
-        ])
+        self.build_packages.extend(["autoconf", "automake", "autopoint", "libtool"])
 
-        if options.install_via == 'destdir':
-            self.install_via_destdir = True
-        elif options.install_via == 'prefix':
-            self.install_via_destdir = False
+        if options.install_via == "destdir":
+            self.options.make_install_var = "DESTDIR"
+        elif options.install_via == "prefix":
+            self.options.make_install_var = ""
         else:
-            raise RuntimeError('Unsupported installation method: "{}"'.format(
-                options.install_via))
+            raise RuntimeError(
+                'Unsupported installation method: "{}"'.format(options.install_via)
+            )
+
+    def enable_cross_compilation(self):
+        pass
 
     def build(self):
-        super().build()
         if not os.path.exists(os.path.join(self.builddir, "configure")):
             generated = False
             scripts = ["autogen.sh", "bootstrap"]
@@ -100,32 +97,41 @@ class AutotoolsPlugin(snapcraft.BasePlugin):
                     continue
                 # Make sure it's executable
                 if not os.access(path, os.X_OK):
-                    os.chmod(path,
-                             stat.S_IRUSR | stat.S_IWUSR | stat.S_IXUSR |
-                             stat.S_IRGRP | stat.S_IWGRP | stat.S_IXGRP |
-                             stat.S_IROTH | stat.S_IWOTH | stat.S_IXOTH)
-                self.run(['env', 'NOCONFIGURE=1', './{}'.format(script)])
+                    os.chmod(
+                        path,
+                        stat.S_IRUSR
+                        | stat.S_IWUSR
+                        | stat.S_IXUSR
+                        | stat.S_IRGRP
+                        | stat.S_IWGRP
+                        | stat.S_IXGRP
+                        | stat.S_IROTH
+                        | stat.S_IWOTH
+                        | stat.S_IXOTH,
+                    )
+                self.run(["env", "NOCONFIGURE=1", "./{}".format(script)])
                 generated = True
                 break
             if not generated:
-                self.run(['autoreconf', '-i'])
+                self.run(["autoreconf", "-i"])
 
-        configure_command = ['./configure']
-        make_install_command = ['make', '-C', 'build', 'install']
+        configure_command = ["./configure"]
 
-        if self.install_via_destdir:
+        if self.options.make_install_var:
             # Use an empty prefix since we'll install via DESTDIR
-            configure_command.append('--prefix=')
-            make_install_command.append('DESTDIR=' + self.installdir)
+            configure_command.append("--prefix=")
         else:
-            configure_command.append('--prefix=' + self.installdir)
+            configure_command.append("--prefix=" + self.installdir)
+        if self.project.is_cross_compiling:
+            configure_command.append("--host={}".format(self.project.arch_triplet))
 
         self.run(configure_command + self.options.configflags)
-        self.run(['make', '-C', 'build', '-j{}'.format(self.parallel_build_count)])
-        self.run(make_install_command)
+        # Change to ./build directory
+        self.options.make_parameters += ['-C', 'build']
+        self.make()
 
     def snap_fileset(self):
         fileset = super().snap_fileset()
         # Remove .la files which don't work when they are moved around
-        fileset.append('-**/*.la')
+        fileset.append("-**/*.la")
         return fileset
